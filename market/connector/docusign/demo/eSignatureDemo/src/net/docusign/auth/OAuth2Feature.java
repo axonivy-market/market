a2c1140fdd76f9b91c4dc9276ce599ed19e7ec02
Reto Weiss <reto.weiss@axonivy.com>
2021-03-04 12:00:37 +0100

    XIVY-5006 Rename interface OAuth2TokenGet to OAuth2TokenRequester

--- market/connector/docusign/demo/eSignatureDemo/src/net/docusign/auth/OAuth2Feature.java
+++ market/connector/docusign/demo/eSignatureDemo/src/net/docusign/auth/OAuth2Feature.java
@@ -15,7 +15,7 @@ import ch.ivyteam.ivy.rest.client.FeatureConfig;
 import ch.ivyteam.ivy.rest.client.authentication.HttpBasicAuthenticationFeature;
 import ch.ivyteam.ivy.rest.client.oauth2.OAuth2BearerFilter;
 import ch.ivyteam.ivy.rest.client.oauth2.OAuth2RedirectErrorBuilder;
-import ch.ivyteam.ivy.rest.client.oauth2.OAuth2TokenGet.AuthContext;
+import ch.ivyteam.ivy.rest.client.oauth2.OAuth2TokenRequester.AuthContext;
 import ch.ivyteam.ivy.rest.client.oauth2.uri.OAuth2CallbackUriBuilder;
 import ch.ivyteam.ivy.rest.client.oauth2.uri.OAuth2UriProperty;
 import ch.ivyteam.ivy.security.ISecurityConstants;
